1fc5ec8703d5719c527b4d6e84808df657df5484
abdeo8431 <abdeo8431@HRH-IC0027340.wrdsb.ca>
2018-02-23 10:21:57 -0500

    idk

--- src/Assignment_2/A2Q3.java
+++ src/Assignment_2/A2Q3.java
@@ -29,50 +29,43 @@ public class A2Q3 {
         //set the colour of speedyboi to black
         speedyboi.setColor(Color.black);
 
-        //while the front of the speedyboi's path is clear, execute this command
-        while(speedyboi.getStreet()>0){
-            if(!speedyboi.isFacingNorth()){
+        //If ever in a positive integer street coordinate, to reach street 0, face north!
+        while (speedyboi.getStreet() > 0) {
+            if (!speedyboi.isFacingNorth()) {
                 speedyboi.turnRight();
-        }else if(speedyboi.isFacingNorth()){
-            speedyboi.move();
-        }
-        if(speedyboi.getStreet() == 0){
-            speedyboi.turnRight();
+            } else if (speedyboi.isFacingNorth()) {
+                speedyboi.move();
+            }
+            if (speedyboi.getStreet() == 0) {
+                speedyboi.turnRight();
             }
         }
-        
-        while(speedyboi.getStreet()<0){
-            if(!speedyboi.isFacingSouth()){
+        //If ever in a negative integer street coordinate, to reach street 0, face south!
+        while (speedyboi.getStreet() < 0) {
+            if (!speedyboi.isFacingSouth()) {
                 speedyboi.turnRight();
-        }else if(speedyboi.isFacingSouth()){
-            speedyboi.move();
-        }
-        if(speedyboi.getStreet() == 0){
-            speedyboi.turnRight();
+            } else if (speedyboi.isFacingSouth()) {
+                speedyboi.move();
+            }
+            if (speedyboi.getStreet() == 0) {
+                speedyboi.turnRight();
             }
         }
-        
-        while(speedyboi.getAvenue()>0){
-            if(!speedyboi.isFacingWest()){
+        //If ever in a positive integer avenue coordinate, to reach avenue 0, face west!
+        while (speedyboi.getAvenue() > 0) {
+            if (!speedyboi.isFacingWest()) {
                 speedyboi.turnRight();
-        }else if(speedyboi.isFacingWest()){
-            speedyboi.move();
-        }        
-
-        }
-        while(speedyboi.getAvenue()<0){
-            if(!speedyboi.isFacingEast()){
-                speedyboi.turnRight();
-        }else if(speedyboi.isFacingEast()){
-            speedyboi.move();
-        }
+            } else if (speedyboi.isFacingWest()) {
+                speedyboi.move();
             }
         }
-        
+        //If ever in a negative integer avenue coordinate, to reach avenue 0, face east!
+        while (speedyboi.getAvenue() < 0) {
+            if (!speedyboi.isFacingEast()) {
+                speedyboi.turnRight();
+            } else if (speedyboi.isFacingEast()) {
+                speedyboi.move();
+            }
         }
-    
-
-        
-
-
-
+    }
+}
