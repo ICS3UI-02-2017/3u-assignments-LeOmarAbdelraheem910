1118e182ffd178b446991e651da4c0165ba82e9a
abdeo8431 <abdeo8431@HRH-IC0027340.wrdsb.ca>
2018-03-01 10:46:58 -0500

    Assignment 4 Question 1 and 2 (partially complete)

---
+++ src/Assignment_4/A4Q2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author abdeo8431
+ */
+public class A4Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        //Prompt the user to enter in a measurement in inches to convert
+        System.out.println("Are you too lazy to do the math? Do you not want to memorize conversions?");
+        System.out.println("");
+    }
+}
