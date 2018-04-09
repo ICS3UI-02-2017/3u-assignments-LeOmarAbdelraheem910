0403a7c20bf55e322caf33d06d2317632c304b91
Omar <Omar@192.168.2.31>
2018-04-08 23:21:12 -0400

    Assignment 7 requires commenting

--- src/Assignment_6/A6Q7.java
+++ src/Assignment_6/A6Q7.java
@@ -5,6 +5,9 @@
  */
 package Assignment_6;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Omar
@@ -15,7 +18,29 @@ public class A6Q7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter in a number n: ");
+        int n = input.nextInt();
+
+       boolean range[] = new boolean[n +1];
+     
+       for(int i=0;i<range.length;i++)
+            range[i] = true;
+        for(int p = 2; p*p <=n; p++)
+        {
+            if(range[p] == true)
+            {
+                for(int i = p*2; i <= n; i += p)
+                    range[i] = false;
+            }
+        }
+         
+        for(int i = 2; i <= n; i++)
+        {
+            if(range[i] == true)
+                System.out.print(i + " ");
+        }
     }
-    
 }
