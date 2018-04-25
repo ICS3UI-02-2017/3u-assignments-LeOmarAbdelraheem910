e66de6b0777536796627253fd810485880277b6b
LeOmarAbdelraheem910 <36271923+LeOmarAbdelraheem910@users.noreply.github.com>
2018-04-25 10:06:23 -0400

    Assignment 7:

--- src/Assignments_7/A7Q5.java
+++ src/Assignments_7/A7Q5.java
@@ -11,11 +11,39 @@ package Assignments_7;
  */
 public class A7Q5 {
 
-    /**
-     * @param args the command line arguments
-     */
+    //Create a method that will generate a random number of asterisks for the range entered.
+    public static int chaotic(int x, int n, int a){
+        //Set up a certain number of lines of asterisks based on user input
+        int specifiedLines = x;
+        int asteriskLines = 0;
+        //Print out as many asterisk lines as specified above
+        while(asteriskLines < specifiedLines){
+            
+            //Use Math.random to generate a random number of asterisks per line based on a range to choose from.
+            randNum = (int)(Math.random()*(n - a + 1))+ a
+            for(i = 0; i = randNum; i++){
+                System.out.print("*")     
+        }
+            asteriskLines = asteriskLines + 1; 
+    }
+        return randNum; 
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        //Welcome the user and ask them to enter the number of lines they want to generate, and the range they want to work in.
+        System.out.println("Hey there partner! Want to see some magic? I can produce lines of asterisks randomly!");
+        System.out.print("You should tell me how many lines you want! : ");
+        int specifiedLines = input.nextInt();
+        
+        System.out.print("Now give me the lowest number in the range you want: ");
+        int lowestNum = input.nextInt();
+        
+        System.out.print("Finally, what is the highest number in the range? : ");
+        int highestNum = input.nextInt();
+        
+        //Finally, call upon the chaotic method to generate the lines.
+        chaotic(specifiedLines, highestNum, lowestNum);
     }
     
 }
